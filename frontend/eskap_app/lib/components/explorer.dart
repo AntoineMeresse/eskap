9f71308cf98744e5372fa24176a49853c742f121
Antoine Meresse <ant.meresse@gmail.com>
2020-11-12 16:50:44 +0100

    feat : topbar widget

--- frontend/eskap_app/lib/components/explorer.dart
+++ frontend/eskap_app/lib/components/explorer.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:eskap_app/components/topbar.dart';
 
 class Explorer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("I'm a Explorer widget"),
+      child: Column(
+        children: [
+          TopBar(),
+        ],
+      ),
     );
   }
 }
---
+++ frontend/eskap_app/lib/components/topbar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height * 0.10,
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    width: MediaQuery.of(context).size.width * 0.80,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        hintText: "Location",
+                        border: InputBorder.none,
+                      ),
+                      readOnly: false, // On peut écrire ou non
+                      onTap: () async {
+                        //TODO
+                      },
+                    ),
+                  ),
+                  VerticalDivider(
+                    thickness: 2,
+                    color: Colors.black,
+                  ),
+                  IconButton(
+                    icon: new Icon(Icons.sort),
+                    onPressed: () {
+                      print("Button pressed");
+                    },
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 0,
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ));
+  }
+}
