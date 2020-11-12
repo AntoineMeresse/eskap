42bfe849d113686838cf2c4482226fc6df48a3d0
antoine <ant.meresse@gmail.com>
2020-11-12 15:01:19 +0100

    feat : create Home Screen widget

---
+++ frontend/eskap_app/lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _widgets = [];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
