99ddfbce7826d9996aa5c83d1472cb3c87b75b4f
Imad <imadaufil@gmail.com>
2020-11-12 16:39:36 +0100

    feat: adding Google Maps keys

--- frontend/eskap_app/ios/Runner/AppDelegate.swift
+++ frontend/eskap_app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyAArYTlLnuNATODZNqBK0oQL2Dg4gvxZU0")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
