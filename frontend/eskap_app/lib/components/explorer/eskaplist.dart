efdbb05e490de5e32ee1d38a98c37a1354e38da3
Antoine Meresse <ant.meresse@gmail.com>
2020-12-09 11:38:41 +0100

    feat: improve escape game list

--- frontend/eskap_app/lib/components/explorer/eskaplist.dart
+++ frontend/eskap_app/lib/components/explorer/eskaplist.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:eskap_app/models/escapeGame.dart';
+import 'dart:math';
 
 class EskapList extends StatefulWidget {
   @override
@@ -8,6 +9,9 @@ class EskapList extends StatefulWidget {
 
 class _EskapListState extends State<EskapList> {
   var eg = [];
+
+  final _saved = Set<String>();
+  Random r = new Random();
 
   void addEscapeGame(String addresse, double lat, double long, String id) {
     setState(() {
@@ -19,17 +23,30 @@ class _EskapListState extends State<EskapList> {
   void initState() {
     super.initState();
     addEscapeGame("test", 2.0, 2.0, "1");
-    addEscapeGame("test2", 2.0, 2.0, "1");
+    addEscapeGame("test2", 2.0, 2.0, "2");
     print(eg);
   }
 
   Widget _buildRow(EscapeGame eg) {
+    final alreadySaved = _saved.contains(eg.id);
+
     return ListTile(
       leading: Icon(Icons.home),
       title: Text("Escape Game"),
       subtitle: Text(eg.addresse),
-      selected: true,
-      onLongPress: () => (addEscapeGame("Lol", 4.0, 4.0, "1")),
+      onLongPress: () =>
+          (addEscapeGame("NEW EG", 4.0, 4.0, r.nextInt(10000).toString())),
+      trailing: IconButton(
+        icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border),
+        onPressed: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(eg.id);
+            } else
+              _saved.add(eg.id);
+          });
+        },
+      ),
     );
   }
 
