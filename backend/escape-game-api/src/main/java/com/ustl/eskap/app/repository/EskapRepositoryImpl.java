796ec3be164d2e81be2b87ced2ea041092340690
Antoine Meresse <ant.meresse@gmail.com>
2021-01-12 16:12:55 +0100

    feat: EskapRepository + implement methods in EskapRepositoryImpl

--- backend/escape-game-api/src/main/java/com/ustl/eskap/app/repository/EskapRepositoryImpl.java
+++ backend/escape-game-api/src/main/java/com/ustl/eskap/app/repository/EskapRepositoryImpl.java
@@ -1,4 +1,38 @@
 package com.ustl.eskap.app.repository;
 
-public class EskapRepositoryImpl {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ustl.eskap.app.bo.EscapeGame;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class EskapRepositoryImpl implements EskapRepository{
+
+    private List<EscapeGame> eskaps;
+
+    public EskapRepositoryImpl() {
+         try {
+             var eskapsStream = this.getClass().getResourceAsStream("/eskaps.json");
+             var objectMapper = new ObjectMapper();
+             var eskapsArray = objectMapper.readValue(eskapsStream, EscapeGame[].class);
+             this.eskaps = Arrays.asList(eskapsArray);
+         }
+         catch (IOException e) {
+             e.printStackTrace();
+         }
+    }
+
+    @Override
+    public EscapeGame findEskapById(int id) {
+        for (EscapeGame eg : eskaps) {
+            if (eg.getId() == id) return eg;
+        }
+        return null;
+    }
+
+    @Override
+    public List<EscapeGame> findAllEskaps() {
+        return eskaps;
+    }
 }
