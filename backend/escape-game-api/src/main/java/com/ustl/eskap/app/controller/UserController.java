79eaed42ce14b73d0f3464e58924c75178ed6afe
Antoine Meresse <ant.meresse@gmail.com>
2021-01-20 11:30:48 +0100

    feat: User Controller

---
+++ backend/escape-game-api/src/main/java/com/ustl/eskap/app/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.ustl.eskap.app.controller;
+
+import com.ustl.eskap.app.bo.user.User;
+import com.ustl.eskap.app.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public User getUserFromId(@PathVariable int id) {
+        return this.userService.getUser(id);
+    }
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+}
