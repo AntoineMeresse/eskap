2457aafdf16afa65d4d04d6484b8d438cf05c3eb
Antoine Meresse <ant.meresse@gmail.com>
2021-01-20 11:23:56 +0100

    feat: User Service

---
+++ backend/escape-game-api/src/main/java/com/ustl/eskap/app/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ustl.eskap.app.service;
+
+import com.ustl.eskap.app.bo.user.User;
+
+import java.util.List;
+
+public interface UserService {
+    User getUser(int id);
+    List<User> getAllUsers();
+}
---
+++ backend/escape-game-api/src/main/java/com/ustl/eskap/app/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ustl.eskap.app.service;
+
+import com.ustl.eskap.app.bo.user.User;
+import com.ustl.eskap.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    protected UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserServiceImpl() {
+
+    }
+
+    @Override
+    public User getUser(int id) {
+        return this.userRepository.findUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepository.findAllUsers();
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
