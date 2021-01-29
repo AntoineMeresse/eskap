1232b0eaa8377d83295ce24a3a25781b6a0fefc7
Antoine Meresse <ant.meresse@gmail.com>
2021-01-29 11:44:17 +0100

    chore: move CommandLineRunner & add previous datas

--- backend/escape-game-api/src/main/java/com/ustl/eskap/app/Application.java
+++ backend/escape-game-api/src/main/java/com/ustl/eskap/app/Application.java
@@ -1,15 +1,7 @@
 package com.ustl.eskap.app;
 
-import com.ustl.eskap.app.bo.eskap.EscapeGame;
-import com.ustl.eskap.app.bo.eskap.Review;
-import com.ustl.eskap.app.repository.EskapRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -31,28 +23,5 @@ public class Application {
     public static void main(String... args){
         SpringApplication.run(Application.class, args);
         printRoutes();
-    }
-
-    @Bean
-    @Autowired
-    public CommandLineRunner demo(EskapRepository repository) {
-        return (args) -> {
-            var review = new Review(1,"1","review 1",4.5,"01/29/2021");
-            var review2 = new Review(2,"1","review 2",4.0,"01/29/2021");
-
-            List<String> theme1 = List.of("Sea");
-            List<String> theme2 = List.of("Jail");
-
-            EscapeGame eg = new EscapeGame(
-                    1, "Eskap Test", "Easy", 20.0, "urltest", "descriptiontest",
-                    1, "rue", "Orchies","France",3.0,2.0, theme1, List.of(review));
-
-            EscapeGame eg2 = new EscapeGame(
-                    2, "Eskap Test", "Easy", 20.0, "urltest", "descriptiontest",
-                    1, "rue", "Lille","France",4.0,1.0, theme2, List.of(review2));
-
-            repository.save(eg);
-            repository.save(eg2);
-        };
     }
 }
