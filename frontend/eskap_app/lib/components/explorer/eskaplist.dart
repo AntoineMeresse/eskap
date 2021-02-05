4d46062fd3242303b8fc7b1a806ced7e3089a47a
Antoine Meresse <ant.meresse@gmail.com>
2021-02-05 15:29:37 +0100

    feat: Listtiles

--- frontend/eskap_app/lib/components/explorer/eskaplist.dart
+++ frontend/eskap_app/lib/components/explorer/eskaplist.dart
@@ -63,47 +63,115 @@ class EskapWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     //ignore: close_sinks
     final EskapBloc eskapbloc = BlocProvider.of<EskapBloc>(context);
-    return ListTile(
-      title: Row(
-        children: [
-          Text(eg.name),
-          if (eg.official) (Icon(Icons.verified_outlined)),
-        ],
-      ),
-      subtitle: RatingBar.builder(
-        initialRating: eg.averageRate(),
-        minRating: 0,
-        ignoreGestures: true,
-        direction: Axis.horizontal,
-        allowHalfRating: true,
-        itemCount: 5,
-        itemSize: 25,
-        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-        itemBuilder: (context, _) => Icon(
-          Icons.star,
-          color: Colors.amber,
+    return Card(
+      child: ListTile(
+        title: Column(
+          children: [
+            title(),
+            theme(),
+          ],
         ),
-        onRatingUpdate: (rating) {},
-      ),
-      trailing: IconButton(
-        icon: Icon(eg.isFav ? Icons.favorite : Icons.favorite_border),
-        onPressed: () {
-          if (eg.isFav) {
-            BlocProvider.of<EskapBloc>(context).add(EskapRemoveFav(eg.id));
-          } else {
-            BlocProvider.of<EskapBloc>(context).add(EskapAddFav(eg.id));
+        subtitle: rateAndLocation(),
+        trailing: favIcon(context),
+        onTap: () {
+          if (eg != null) {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => BlocProvider.value(
+                          value: eskapbloc,
+                          child: EskapInfo(eg: eg),
+                        )));
           }
         },
       ),
-      onTap: () {
-        if (eg != null) {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => BlocProvider.value(
-                        value: eskapbloc,
-                        child: EskapInfo(eg: eg),
-                      )));
+    );
+  }
+
+  Widget title() {
+    return Row(
+      children: [
+        Text(
+          eg.name,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        if (eg.official)
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Icon(Icons.verified_outlined),
+          ),
+      ],
+    );
+  }
+
+  Widget theme() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Row(
+        children: [
+          Text("Thème(s) :"),
+          Text("Theme list"),
+        ],
+      ),
+    );
+  }
+
+  Widget rateAndLocation() {
+    return Row(
+      children: [
+        rateStars(),
+        Text(
+          " ( ${eg.reviews.length} ) ",
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+        Text(
+          " | ",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Icon(Icons.location_pin),
+        Text('${eg.city}, ${eg.country}'),
+      ],
+    );
+  }
+
+  Widget imageEskap() {
+    return CircleAvatar(
+        backgroundImage: NetworkImage(
+            'https://cdn.pixabay.com/photo/2016/01/22/11/50/live-escape-game-1155620_960_720.jpg'),
+        radius: 30);
+  }
+
+  Widget rateStars() {
+    return RatingBar.builder(
+      initialRating: eg.averageRate(),
+      minRating: 0,
+      ignoreGestures: true,
+      direction: Axis.horizontal,
+      allowHalfRating: true,
+      itemCount: 5,
+      itemSize: 15,
+      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+      itemBuilder: (context, _) => Icon(
+        Icons.star,
+        color: Colors.amber,
+      ),
+      onRatingUpdate: (rating) {},
+    );
+  }
+
+  Widget favIcon(BuildContext context) {
+    return IconButton(
+      icon: Icon(eg.isFav ? Icons.favorite : Icons.favorite_border),
+      onPressed: () {
+        if (eg.isFav) {
+          BlocProvider.of<EskapBloc>(context).add(EskapRemoveFav(eg.id));
+        } else {
+          BlocProvider.of<EskapBloc>(context).add(EskapAddFav(eg.id));
         }
       },
     );
